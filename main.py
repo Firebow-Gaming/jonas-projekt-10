e856d251ecfb85b31f6cd24e149063e2b7298c60
Firebow-Gaming <131754902+Firebow-Gaming@users.noreply.github.com>
2023-05-10 13:13:03 +0200

    JonasGame

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
+def on_button_a():
+    Korp.change(LedSpriteProperty.X, -1)
+input.on_button_event(Button.A, input.button_event_click(), on_button_a)
+
+def on_button_b():
+    Korp.change(LedSpriteProperty.X, 1)
+input.on_button_event(Button.B, input.button_event_click(), on_button_b)
+
+Ball: game.LedSprite = None
+Korp: game.LedSprite = None
+Korp = game.create_sprite(2, 4)
+game.set_life(3)
+
 def on_forever():
-    basic.show_leds("""
-        # # # # .
-                # # # # .
-                # # # # .
-                # # # # .
-                # # # # .
-    """)
-    basic.show_string("Hurensohn")
-    music.play_melody("B B E G D B B F ", 120)
-    music.play_melody("F F A E E B F F ", 120)
+    global Ball
+    Ball = game.create_sprite(randint(0, 4), 0)
+    for index in range(4):
+        Ball.change(LedSpriteProperty.Y, 1)
+        basic.pause(400)
+    if Korp.is_touching(Ball):
+        game.add_score(1)
+        Ball.delete()
+    else:
+        Ball.delete()
+        game.remove_life(1)
 basic.forever(on_forever)
