76c442468bc58a4a8bf86a65b987724f07452d15
Firebow-Gaming <131754902+Firebow-Gaming@users.noreply.github.com>
2023-06-01 22:31:47 +0200

    Aktualisiere main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,26 +1,111 @@
-def on_button_a():
+def on_pin_touch_p0():
     Korp.change(LedSpriteProperty.X, -1)
-input.on_button_event(Button.A, input.button_event_click(), on_button_a)
+input.on_pin_touch_event(TouchPin.P0, input.button_event_down(), on_pin_touch_p0)
 
-def on_button_b():
+def on_pin_touch_p3():
     Korp.change(LedSpriteProperty.X, 1)
-input.on_button_event(Button.B, input.button_event_click(), on_button_b)
+input.on_pin_touch_event(TouchPin.P3, input.button_event_down(), on_pin_touch_p3)
 
+sprite = 0
 Ball: game.LedSprite = None
+Game_20 = 0
+Musik = 0
 Korp: game.LedSprite = None
-Korp = game.create_sprite(2, 4)
+Menü = 1
+Game = 0
 game.set_life(3)
+game.set_score(1)
 
 def on_forever():
-    global Ball
-    Ball = game.create_sprite(randint(0, 4), 0)
-    for index in range(4):
-        Ball.change(LedSpriteProperty.Y, 1)
-        basic.pause(400)
-    if Korp.is_touching(Ball):
-        game.add_score(1)
-        Ball.delete()
-    else:
-        Ball.delete()
-        game.remove_life(1)
+    global Menü, Musik
+    if Menü == 1:
+        basic.show_leds("""
+            . # # # .
+                        . # . . .
+                        . # # . .
+                        . # . . .
+                        . # # # .
+        """)
+        if input.button_is_pressed(Button.A):
+            Menü = 2
+        if input.pin_is_pressed(TouchPin.P1):
+            Musik = 1
 basic.forever(on_forever)
+
+def on_forever2():
+    global Korp
+    if Game_20 == 2:
+        Korp = game.create_sprite(2, 4)
+basic.forever(on_forever2)
+
+def on_forever3():
+    global Menü
+    if Menü == 3:
+        basic.show_leds("""
+            . # . # .
+                        . # . # .
+                        . # # # .
+                        . # . # .
+                        . # . # .
+        """)
+        if input.button_is_pressed(Button.B):
+            Menü = 2
+basic.forever(on_forever3)
+
+def on_forever4():
+    global Menü, Musik
+    if Menü == 2:
+        basic.show_leds("""
+            # . . . #
+                        # # . # #
+                        # . # . #
+                        # . . . #
+                        # . . . #
+        """)
+        if input.button_is_pressed(Button.A):
+            Menü = 3
+        if input.button_is_pressed(Button.B):
+            Menü = 1
+        if input.pin_is_pressed(TouchPin.P1):
+            Musik = 2
+basic.forever(on_forever4)
+
+def on_forever5():
+    if Musik == 2:
+        music.play_melody("C D E F D E F C5 ", 200)
+        music.play_melody("C F E D E F E C ", 200)
+basic.forever(on_forever5)
+
+def on_forever6():
+    if Musik == 1:
+        music.play_melody("C5 F G A F G A C ", 140)
+        music.play_melody("C5 G F E G A B C ", 140)
+basic.forever(on_forever6)
+
+def on_forever7():
+    global Game, Game_20, Menü
+    if input.pin_is_pressed(TouchPin.P1):
+        Game = 2
+        Game_20 += 2
+        Menü = 0
+basic.forever(on_forever7)
+
+def on_forever8():
+    pass
+basic.forever(on_forever8)
+
+def on_forever9():
+    global Ball, sprite
+    if Game == 2:
+        Ball = game.create_sprite(randint(0, 4), 0)
+        for index in range(4):
+            Ball.change(LedSpriteProperty.Y, 1)
+            basic.pause(500)
+        if Korp.is_touching(Ball):
+            game.add_score(1)
+            Ball.delete()
+        else:
+            Ball.delete()
+            game.remove_life(1)
+        sprite += 0
+basic.forever(on_forever9)
--- main.ts
+++ main.ts
@@ -1,24 +1,154 @@
-input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+input.onPinTouchEvent(TouchPin.P0, input.buttonEventDown(), function () {
     Korp.change(LedSpriteProperty.X, -1)
 })
-input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+input.onPinTouchEvent(TouchPin.P3, input.buttonEventDown(), function () {
     Korp.change(LedSpriteProperty.X, 1)
 })
+let sprite = 0
 let Ball: game.LedSprite = null
+let Game = 0
+let Musik = 0
+let Game_20 = 0
 let Korp: game.LedSprite = null
-Korp = game.createSprite(2, 4)
+let Lobbymusik = 1
+let Menü = 1
 game.setLife(3)
+game.setScore(1)
 basic.forever(function () {
-    Ball = game.createSprite(randint(0, 4), 0)
-    for (let index = 0; index < 4; index++) {
-        Ball.change(LedSpriteProperty.Y, 1)
-        basic.pause(400)
+    if (Game_20 == 2) {
+        Korp = game.createSprite(2, 4)
     }
-    if (Korp.isTouching(Ball)) {
-        game.addScore(1)
-        Ball.delete()
-    } else {
-        Ball.delete()
-        game.removeLife(1)
+})
+basic.forever(function () {
+    if (Menü == 1) {
+        basic.showLeds(`
+            . # # # .
+            . # . . .
+            . # # . .
+            . # . . .
+            . # # # .
+            `)
+        if (input.buttonIsPressed(Button.A)) {
+            Menü = 2
+        }
+        if (input.pinIsPressed(TouchPin.P1)) {
+            Musik = 1
+            Lobbymusik = 0
+        }
+    }
+})
+basic.forever(function () {
+    if (Game == 2) {
+        Ball = game.createSprite(randint(0, 4), 0)
+        for (let index = 0; index < 4; index++) {
+            Ball.change(LedSpriteProperty.Y, 1)
+            basic.pause(500)
+        }
+        if (Korp.isTouching(Ball)) {
+            game.addScore(1)
+            Ball.delete()
+        } else {
+            Ball.delete()
+            game.removeLife(1)
+        }
+        sprite += 0
+    }
+})
+basic.forever(function () {
+    if (Menü == 3) {
+        basic.showLeds(`
+            . # . # .
+            . # . # .
+            . # # # .
+            . # . # .
+            . # . # .
+            `)
+        if (input.buttonIsPressed(Button.B)) {
+            Menü = 2
+        }
+        if (input.pinIsPressed(TouchPin.P1)) {
+            Lobbymusik = 0
+            Musik = 3
+        }
+        if (input.buttonIsPressed(Button.A)) {
+            Menü = 4
+        }
+    }
+})
+basic.forever(function () {
+    if (Menü == 2) {
+        basic.showLeds(`
+            # . . . #
+            # # . # #
+            # . # . #
+            # . . . #
+            # . . . #
+            `)
+        if (input.buttonIsPressed(Button.A)) {
+            Menü = 3
+        }
+        if (input.buttonIsPressed(Button.B)) {
+            Menü = 1
+        }
+        if (input.pinIsPressed(TouchPin.P1)) {
+            Lobbymusik = 0
+            Musik = 2
+        }
+    }
+})
+basic.forever(function () {
+    if (Menü == 4) {
+        basic.showLeds(`
+            . # # . .
+            # . . # .
+            # . . . .
+            # . . # .
+            . # # . .
+            `)
+        if (input.buttonIsPressed(Button.B)) {
+            Menü = 3
+        }
+        if (input.buttonIsPressed(Button.A)) {
+            basic.showString("Kolja")
+        }
+    }
+})
+basic.forever(function () {
+    if (Musik == 2) {
+        music.playMelody("C D E F D E F C5 ", 200)
+        music.playMelody("C F E D E F E C ", 200)
+        if (game.score() == 50) {
+            music.playMelody("C D C5 D C5 D C5 D ", 500)
+        }
+    }
+})
+basic.forever(function () {
+    if (Musik == 1) {
+        music.playMelody("C5 F G A F G A C ", 140)
+        music.playMelody("C5 G F E G A B C ", 140)
+        if (game.score() == 50) {
+            music.playMelody("C D C5 D C5 D C5 D ", 500)
+        }
+    }
+})
+basic.forever(function () {
+    if (input.pinIsPressed(TouchPin.P1)) {
+        Game = 2
+        Game_20 += 2
+        Menü = 0
+    }
+})
+basic.forever(function () {
+    if (Musik == 3) {
+        music.playMelody("C D E C5 C D E C ", 240)
+        music.playMelody("C5 E F E D C D E ", 240)
+        if (game.score() == 50) {
+            music.playMelody("C D C5 D C5 D C5 D ", 500)
+        }
+    }
+})
+basic.forever(function () {
+    if (Lobbymusik == 1) {
+        music.playMelody("G A B A F G A G ", 100)
     }
 })
